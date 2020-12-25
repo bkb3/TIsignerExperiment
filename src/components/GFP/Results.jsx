fc0d6e0d41e0377ebab74e6c5abd51cabeb3302e
bkb3 <30164638+bkb3@users.noreply.github.com>
2020-12-26 11:36:57 +1300

    fix chartjs problems

--- src/components/GFP/Results.jsx
+++ src/components/GFP/Results.jsx
@@ -66,6 +66,7 @@ class Results extends Component {
       sliderDefaultPosition: 15,
       sliderCurrentPosition: 15,
       fixYAxis: true,
+      showAllConstructs: false,
       time: null,
     };
     this.plot = this.plot.bind(this);
@@ -89,6 +90,15 @@ class Results extends Component {
     );
   };
 
+  handleChangeShowAllConstructs = (e) => {
+    this.setState(
+      {
+        showAllConstructs: !this.state.showAllConstructs,
+      },
+      () => this.plot()
+    );
+  };
+
   plot = () => {
     let i = this.state.sliderCurrentPosition;
 
@@ -98,7 +108,12 @@ class Results extends Component {
     let nucleotideSequence = fluorescence.map((a) => a["First 30 nt"]);
 
     let nativeGFP = nucleotideSequence[0];
+
     let currentFluorescence = fluorescence.map(
+      (a) => a["Data"][i]["All Fluorescence"]
+    );
+
+    let currentMeanFluorescence = fluorescence.map(
       (a) => a["Data"][i]["Mean Fluorescence"]
     );
 
@@ -114,9 +129,69 @@ class Results extends Component {
           label: "GFP Fluorescence",
           data: expressionScore.map((v, j) => ({
             x: v,
-            y: currentFluorescence[j],
-            yMin: currentFluorescence[j] - currentStdFluorescence[j],
-            yMax: currentFluorescence[j] + currentStdFluorescence[j],
+            y: currentMeanFluorescence[j],
+            yMin: currentMeanFluorescence[j] - currentStdFluorescence[j],
+            yMax: currentMeanFluorescence[j] + currentStdFluorescence[j],
+          })),
+          pointRadius: function (context) {
+            var index = context.dataIndex;
+            return index === 0 ? 5 : 2;
+          },
+          pointHoverRadius: 7,
+        },
+        {
+          label: "GFP Fluorescence 1",
+          data: expressionScore.map((v, j) => ({
+            x: v,
+            y: currentFluorescence[j][0],
+          })),
+          pointRadius: function (context) {
+            var index = context.dataIndex;
+            return index === 0 ? 5 : 2;
+          },
+          pointHoverRadius: 7,
+        },
+        {
+          label: "GFP Fluorescence 2",
+          data: expressionScore.map((v, j) => ({
+            x: v,
+            y: currentFluorescence[j][1],
+          })),
+          pointRadius: function (context) {
+            var index = context.dataIndex;
+            return index === 0 ? 5 : 2;
+          },
+          pointHoverRadius: 7,
+        },
+        {
+          label: "GFP Fluorescence 3",
+          data: expressionScore.map((v, j) => ({
+            x: v,
+            y: currentFluorescence[j][2],
+          })),
+          pointRadius: function (context) {
+            var index = context.dataIndex;
+            return index === 0 ? 5 : 2;
+          },
+          pointHoverRadius: 7,
+        },
+        {
+          label: "GFP Fluorescence 4",
+          data: expressionScore.map((v, j) => ({
+            x: v,
+            y: currentFluorescence[j][3],
+          })),
+          pointRadius: function (context) {
+            var index = context.dataIndex;
+            return index === 0 ? 5 : 2;
+          },
+          pointHoverRadius: 7,
+        },
+        {
+          label: "GFP Fluorescence 5",
+          data: expressionScore.map((v, j) => ({
+            x: v,
+            y: currentFluorescence[j][4],
           })),
           pointRadius: function (context) {
             var index = context.dataIndex;
@@ -192,113 +267,116 @@ class Results extends Component {
     this.plot();
   }
 
-
   render() {
     const { classes } = this.props;
     return (
       <Fragment>
-      <div className={classes.root}>
-        <Typography variant="h5" gutterBottom>
-          GFP results
-        </Typography>
-
-        <Typography variant="body1" gutterBottom>
-          Use the slider
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Fluorescence is normalised by ....
-        </Typography>
-
-        <Slider
-          defaultValue={this.state.sliderDefaultPosition}
-          step={1}
-          min={0}
-          max={23}
-          onChange={this.handleSliderChange}
-          valueLabelDisplay="auto"
-          key="GFP Slider"
-        />
-
-        <Grid container direction="row" justify="center">
-          <Typography variant="h6" component="h2" gutterBottom>
-            {this.state.time ? `Time: ${this.state.time}` : null}
+        <div className={classes.root}>
+          <Typography variant="h5" gutterBottom>
+            GFP results
           </Typography>
-        </Grid>
 
-        <Grid container direction="row"  alignItems="stretch">
-          <Grid item md={6} sm={9} xs={9} className={classes.gridItem}>
-            <div className={classes.paper}>
-              <Scatter
-                data={this.state.data}
-                options={this.state.options}
-                // getElementAtEvent={getElementAtEvent}
-                width={350}
-                height={200}
-              />
-            </div>
+          <Typography variant="body1" gutterBottom>
+            Use the slider
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Fluorescence is normalised by ....
+          </Typography>
+
+          <Slider
+            defaultValue={this.state.sliderDefaultPosition}
+            step={1}
+            min={0}
+            max={23}
+            onChange={this.handleSliderChange}
+            valueLabelDisplay="auto"
+            key="GFP Slider"
+          />
+
+          <Grid container direction="row" justify="center">
+            <Typography variant="h6" component="h2" gutterBottom>
+              {this.state.time ? `Time: ${this.state.time}` : null}
+            </Typography>
           </Grid>
-          <Grid item md={6} sm={9} xs={9} className={classes.gridItem}>
-            <div className={classes.paper}>
-              {/* <Typography
+
+          <Grid container direction="row" alignItems="stretch">
+            <Grid item md={6} sm={9} xs={9} className={classes.gridItem}>
+              <div className={classes.paper}>
+                <Scatter
+                  data={this.state.data}
+                  options={this.state.options}
+                  // getElementAtEvent={getElementAtEvent}
+                  width={350}
+                  height={200}
+                />
+              </div>
+            </Grid>
+            <Grid item md={6} sm={9} xs={9} className={classes.gridItem}>
+              <div className={classes.paper}>
+                {/* <Typography
                 variant="h6"
                 gutterbottom="true"
                 className={classes.typography}
               >
                 Click on a point to view the sequence.
               </Typography> */}
-                            <Scatter
-                data={this.state.data}
-                options={this.state.options}
-                // getElementAtEvent={getElementAtEvent}
-                width={350}
-                height={200}
-              />
-            </div>
-          </Grid>
-        </Grid>
-        <Grid container direction="row" justify="center">
-          <FormControl component="fieldset" className={classes.paper}>
-            <FormLabel component="legend">Plot Options</FormLabel>
-            <FormGroup row>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={this.state.fixYAxis}
-                    onClick={this.handleChangeFixYAxisSwitch}
-                    name="fixYAxis"
-                  />
-                }
-                label="Fix Y-axis"
-              />
-              {/* <FormControlLabel
-              control={
-                <Switch
-                  // checked={state.jason}
-                  // onChange={handleChange}
-                  name="Option 2"
+                <Scatter
+                  data={this.state.data}
+                  options={this.state.options}
+                  // getElementAtEvent={getElementAtEvent}
+                  width={350}
+                  height={200}
                 />
-              }
-              label="Option 2"
-            /> */}
-            </FormGroup>
-            {/* <FormHelperText>Help text</FormHelperText> */}
-          </FormControl>
-        </Grid>
+              </div>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justify="center">
+            <FormControl component="fieldset" className={classes.paper}>
+              <FormLabel component="legend">Plot Options</FormLabel>
+              <FormGroup row>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.fixYAxis}
+                      onClick={this.handleChangeFixYAxisSwitch}
+                      name="fixYAxis"
+                    />
+                  }
+                  label="Fix Y-axis"
+                />
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.showAllConstructs}
+                      onChange={this.handleChangeShowAllConstructs}
+                      name="ShowAllConstructs"
+                    />
+                  }
+                  label={
+                    !this.state.showAllConstructs
+                      ? "See all constructs"
+                      : "See only mean"
+                  }
+                />
+              </FormGroup>
+              {/* <FormHelperText>Help text</FormHelperText> */}
+            </FormControl>
+          </Grid>
 
-        <Grid container direction="row" justify="center">
-          <Button
-            color="default"
-            className={classes.button}
-            startIcon={<GetAppIcon />}
-            // href={`data:text/json;charset=utf-8,${encodeURIComponent(
-            //   JSON.stringify(sequences)
-            // )}`}
-            download="TIsigner_GFP_constructs.json"
-          >
-            Download Constructs (JSON)
-          </Button>
-        </Grid>
-      </div>
+          <Grid container direction="row" justify="center">
+            <Button
+              color="default"
+              className={classes.button}
+              startIcon={<GetAppIcon />}
+              // href={`data:text/json;charset=utf-8,${encodeURIComponent(
+              //   JSON.stringify(sequences)
+              // )}`}
+              download="TIsigner_GFP_constructs.json"
+            >
+              Download Constructs (JSON)
+            </Button>
+          </Grid>
+        </div>
       </Fragment>
     );
   }
