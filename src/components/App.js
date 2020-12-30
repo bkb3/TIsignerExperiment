4f8e88b8f7f0fd16c80c7e094cf6bb839cb1fe7b
bkb3 <30164638+bkb3@users.noreply.github.com>
2020-12-30 13:53:06 +1300

    travis ci fixes

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ const notFoundMessage = "We did not find anything here!"
 
 function App() {
   return (
-    <Router basename="/TIsignerExperiment">
+    <Router basename="/">
       <ErrorBoundary>
       <Suspense fallback={<Main page={<Loader />}/>}>
       <Switch>
